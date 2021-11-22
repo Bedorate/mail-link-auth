45676d71f8b21ae717b2beba73ce0d16974e1b48
ShusakuFukami <beet.shudora@docomo.ne.jp>
2021-11-22 21:57:10 +0900

    uidをfirestoreに追加

--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -7,11 +7,28 @@ import "firebase/auth";
 import "firebase/firestore";
 
 export const actions: ActionTree<IauthState, RootState> = {
-  signUp({dispatch},payload:{id:string; password:string}):void{
+  signUp({ dispatch }, payload: { id: string; password: string }): void {
     firebase.auth().createUserWithEmailAndPassword(payload.id, payload.password)
-    .then()
-    .catch(error => {
-      alert(error.message);
-    })
+      .then(
+        async user => {
+          dispatch("initUserDocument", user); //作成後すぐにdbに追加
+        })
+      .catch(error => {
+        alert(error.message);
+      })
   },
+  signIn({ }, payload: { id: string; password: string }): void {
+    firebase.auth().signInWithEmailAndPassword(payload.id, payload.password)
+      .then()
+      .catch(error => {
+        alert(error.message);
+      })
+  },
+  async initUserDocument({ }, payload: { user: firebase.User }): Promise<void> {
+    console.log(payload);
+    firebase.firestore().collection("user").doc(payload.user.uid)
+    .set({
+      uid: payload.user.uid
+    })
+  }
 };
